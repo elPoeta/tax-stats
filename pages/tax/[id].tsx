0a24918241aa24ff9e5698a999e9ceb0b879a5b7
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-10-02 16:30:48 -0300

    fix date tax form - add input date  to tax

--- pages/tax/[id].tsx
+++ pages/tax/[id].tsx
@@ -7,6 +7,8 @@ import { useTheme } from "../../context/theme/useTheme";
 import supabase from "../../supabase/supabaseClient";
 import { ITax } from "../../interfaces/ITax";
 import { DynamicChart } from "../../components/chart/DynamicChart";
+import { getMonth } from "../../utils/getMonth";
+import { prepareServerlessUrl } from "next/dist/server/base-server";
 
 const fetchData = async (id: string) => {
   const year = new Date().getFullYear();
@@ -44,7 +46,16 @@ const ViewTax = ({
       ? `Servicios - ${label}`
       : `${!taxes.length ? "" : taxes[0].name} - ${label}`
   );
-
+  const [dateForm, setDateForm] = useState({
+    from: `${new Date().getFullYear()}-${
+      getMonth(new Date().getMonth())!.mm
+    }-${new Date().getDate() > 9 ? new Date().getDate() : `0${new Date().getDate()}`}`,
+    to: `${new Date().getFullYear()}-${
+      getMonth(new Date().getMonth())!.mm
+    }-${new Date().getDate() > 9 ? new Date().getDate() : `0${new Date().getDate()}`}`
+  });
+  // const minDate = new Date()
+  // const maxDate = new Date(minDate.setMonth(min.getMonth()+12));
   console.log("TAXES ", taxes);
   if (error) {
     toast.error(error.message, {
@@ -55,6 +66,18 @@ const ViewTax = ({
 
   const handleChange = (ev: React.ChangeEvent<HTMLSelectElement>) => {
     setCurrentChart(ev.target.value);
+  };
+
+  const handleDate = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value} = ev.target;
+
+    setDateForm(prev => {
+      console.log('PREV',prev )
+      return {
+        ...prev,
+        [name]: value
+      };
+    });
   };
 
   const chart = () => (
@@ -78,21 +101,52 @@ const ViewTax = ({
       {chart()}
       <div className="pt-5">
         {taxes.length > 0 && (
-          <select
-            className="dark:bg-slate-800 rounded-full border-2 border-blue-400 p-2"
-            id="chartSelection"
-            name="chartSelection"
-            value={currentChart}
-            onChange={handleChange}
-          >
-            {["DOUGHNUT", "PIE", "BAR", "LINE", "POLAR", "RADAR"].map(
-              (type) => (
-                <option key={type} value={type}>
-                  {type}
-                </option>
-              )
-            )}
-          </select>
+          <div>
+            <select
+              className="dark:bg-slate-800 rounded-full border-2 border-blue-400 p-2"
+              id="chartSelection"
+              name="chartSelection"
+              value={currentChart}
+              onChange={handleChange}
+            >
+              {["DOUGHNUT", "PIE", "BAR", "LINE", "POLAR", "RADAR"].map(
+                (type) => (
+                  <option key={type} value={type}>
+                    {type}
+                  </option>
+                )
+              )}
+            </select>
+            <div className="flex m-3 flex-col">
+              <label htmlFor="from">
+                From:
+                <input
+                  className="dark:bg-slate-800 rounded-full border-2 border-blue-400 p-2"
+                  type="date"
+                  id="from"
+                  name="from"
+                  value={dateForm.from}
+                  onChange={handleDate}
+                />
+              </label>
+              <label htmlFor="to">
+                To:
+                <input
+                 className="dark:bg-slate-800 rounded-full border-2 border-blue-400 p-2"
+                  type="date"
+                  id="to"
+                  name="to"
+                  value={dateForm.to}
+                  onChange={handleDate}
+                />
+              </label>
+            </div>
+            <pre>
+              
+            {JSON.stringify(dateForm, null, 2)}
+            </pre>
+            <button>Search</button>
+          </div>
         )}
       </div>
     </div>
