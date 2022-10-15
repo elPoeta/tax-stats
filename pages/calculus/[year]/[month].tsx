76a2cef1c07578ad3ca1592bb53e17220ddd1b64
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-10-15 15:05:54 -0300

    add months items by year

--- pages/calculus/[year]/[month].tsx
+++ pages/calculus/[year]/[month].tsx
@@ -1,8 +1,11 @@
+import { useRouter } from 'next/router';
 import React from 'react'
 
 const Month = () => {
+  const router = useRouter();
+  const { month } = router.query;
   return (
-    <div>Month</div>
+    <div>{month}</div>
   )
 }
 
