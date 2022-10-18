5c9d0c3c16f21c82473c2b57b5665b725a04aac5
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-10-18 08:58:24 -0300

    create taxes table

--- pages/calculus/[year]/[month].tsx
+++ pages/calculus/[year]/[month].tsx
@@ -35,7 +35,21 @@ const Month = ({
   return (
     <div>
       <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold capitalize text-center py-2'>{month} - {year}</h2>
-      <div className='grid '></div>
+      <div className='flex flex-col border-t-2 border-l-2 border-r-2 rounded-md border-[#0ea5e9] w-[100%] md:w-[80%] lg:w-[60%] container my-0 mx-auto'>
+        <div className='grid grid-cols-[10%_50%_40%] text-xl items-center justify-center border-b-2 border-[#0ea5e9] font-bold text-[#0ea5e9] h-[40px]'> 
+          <h4 className='h-[40px] border-r-2  border-[#0ea5e9] text-center flex items-center justify-center'>Id</h4>
+          <h4 className='h-[40px] border-r-2  border-[#0ea5e9] text-center flex items-center justify-center'>Servicio</h4>
+          <h4 className='h-[40px] text-center flex items-center justify-center'>Monto</h4>
+        </div>
+
+        {taxes.sort((a,b) => (a.tax_type_id - b.tax_type_id )).map(tax => (
+          <div key={tax.tax_type_id} className='grid grid-cols-[10%_50%_40%] text-xl items-center justify-center border-b-2 border-[#0ea5e9] rounded-md font-semibold'>
+            <h4 className='h-[40px] border-r-2  border-[#0ea5e9] text-center flex items-center justify-center'>{tax.tax_type_id}</h4>
+            <h4 className='h-[40px] border-r-2  border-[#0ea5e9] text-center flex items-center justify-center'>{tax.name}</h4>
+            <h4 className='h-[40px] text-center flex items-center justify-center'>{tax.amount}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
